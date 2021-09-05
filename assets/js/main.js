3380f086c848fc145cc5e49f99593e07079ac2e4
yigituygun8 <87874300+yigituygun8@users.noreply.github.com>
2021-09-05 22:36:08 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@
 */
 
 (function($) {
+	
+	var siteWidth = 1280;
+	var scale = screen.width /siteWidth;
+
+	document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
 	var	$window = $(window),
 		$body = $('body'),
